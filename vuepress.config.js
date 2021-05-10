b13cd77e728874f64ea66c923c8304cfc0aac8a7
hwjoy <hwjoy@163.com>
2021-05-10 22:13:46 +0800

    Initial commit

---
+++ vuepress.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  // 站点配置
+  base: './',
+  lang: 'zh-CN',
+  title: '你好， Clog ！',
+  description: '这是一个 Clog 站点',
+
+  // 主题配置
+  themeConfig: {
+    logo: 'https://avatars.githubusercontent.com/u/8112136',
+  },
+
+  // Dev config
+  port: 8100,
+}
